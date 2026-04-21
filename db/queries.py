89897335cc3f3a60370f6b4958b3cca14b96ca37
Joomart <zhoomart.konushbekovich@gmail.com>
2026-04-21 23:42:18 +0600

    homework

---
+++ db/queries.py
@@ -0,0 +1,25 @@
+tasks_table = """
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL,
+        completed INTEGER DEFAULT 0
+    )
+"""
+
+
+
+insert_task = 'INSERT INTO tasks (task) VALUES (?)'
+
+
+select_tasks = 'SELECT * FROM tasks'
+
+select_tasks_completed = 'SELECT * FROM tasks WHERE completed = 1'
+
+select_tasks_uncompleted = 'SELECT * FROM tasks WHERE completed = 0'
+
+
+update_task = 'UPDATE tasks SET task = ? WHERE id = ?'
+
+
+
+delete_task = 'DELETE FROM tasks WHERE id = ?'
